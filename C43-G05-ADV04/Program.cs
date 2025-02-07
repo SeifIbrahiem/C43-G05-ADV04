9aa1645c6f8fd2586e1f03714eae93c27dc54255
Greg Low <greg@gmail.com>
2025-02-07 11:21:40 +0200

    overview wih hashtable

--- C43-G05-ADV04/Program.cs
+++ C43-G05-ADV04/Program.cs
@@ -5,11 +5,27 @@ namespace C43_G05_ADV04
     internal class Program
     {
         static void Main()
-        { 
-            Ball ball = new Ball() {Id = 1};
-            Console.WriteLine(ball);
-            ball.Location = new Location() { X = 0, Y = 0 , Z=0 };
-            Console.WriteLine(ball);
+        {
+            #region example1
+            //Ball ball = new Ball() { Id = 1 };
+            //Console.WriteLine(ball);
+            //ball.Location = new Location() { X = 0, Y = 0, Z = 0 };
+            //Console.WriteLine(ball); 
+            #endregion
+
+            #region overview wih hashtable
+            //collection : Built_in_datstructure in c#
+            //key : must be unique / not allow null 
+            //===================================================//
+            //Non_generic hashtable : hashtable 
+            //based on object with in (key , value ) with type object 
+            //classhashtable : use double hashing 
+            //عشان نقلل احتماليه تكرار index
+            //there are hashsize<buckets>in the table
+            //hashtable based on array of buckets
+            //Hashtable hashtable = new Hashtable 
+            #endregion
+
         }
     }
 }
