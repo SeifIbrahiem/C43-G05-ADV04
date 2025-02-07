bc9d50f8e92647a4b749f753cf2fc0430e66ea6f
Greg Low <greg@gmail.com>
2025-02-07 12:02:44 +0200

    ex1 with hashtable

--- C43-G05-ADV04/Program.cs
+++ C43-G05-ADV04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using C43_G05_ADV04.Fifa;
+using System.Collections;
 
 namespace C43_G05_ADV04
 {
@@ -24,6 +25,34 @@ namespace C43_G05_ADV04
             //there are hashsize<buckets>in the table
             //hashtable based on array of buckets
             //Hashtable hashtable = new Hashtable 
+            #endregion
+
+            #region ex1 with hashtable
+
+            Hashtable PhoneBook=new Hashtable();
+            PhoneBook.Add("seif", 1111);
+            PhoneBook.Add("ali", 22222);
+            PhoneBook.Add("alia", 333322);
+            PhoneBook.Add("amer", 2888822);
+            Console.WriteLine(PhoneBook.Count);
+            Console.WriteLine(PhoneBook.IsFixedSize);
+            Console.WriteLine(PhoneBook.IsReadOnly);
+            foreach (DictionaryEntry item in PhoneBook) 
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in PhoneBook.Values)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in PhoneBook.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            //contain==containkey
+            //Var x = 12 ;
+            //Var x = 12.5 ;
+            //Var x = "ali" ;
             #endregion
 
         }
